c3915ea7a5c91e37861e58b90a43040263be50d0
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2025-09-11 21:48:51 +0100

    feat: rss feed + permalink change

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,23 @@
+import type { APIContext } from 'astro';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context: APIContext) {
+
+  const posts = await getCollection('posts');
+
+  return rss({
+    title: "Karl's blog",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquam.",
+    site: context.site ?? "https://blog.karlmantle.workers.dev/",
+    trailingSlash: false,
+    stylesheet: '/rss/pretty-feed-v3.xsl',
+    customData: `<language>en-GB</language>`,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `https://blog.karlmantle.workers.dev/posts/${post.data.slug}`,
+    })),
+  });
+}
